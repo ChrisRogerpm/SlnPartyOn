8c93c196b5a6f018a4c59c8e557553051d757e0f
Christian <ChristianRogerpm@gmail.com>
2018-10-22 18:00:14 -0500

    Ok

--- SlnPartyOn/App_Start/RouteConfig.cs
+++ SlnPartyOn/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SlnPartyOn
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Usuario", action = "LoginVista", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SlnPartyOn/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SlnPartyOn.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult LoginVista()
+        {
+            return View("~/Views/Seguridad/Login.cshtml");
+        }
+        public ActionResult LoginRegistroVista()
+        {
+            return View("~/Views/Seguridad/RegistrarLogin.cshtml");
+        }
+    }
+}
